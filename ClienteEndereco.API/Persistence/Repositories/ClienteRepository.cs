ea7075b7f2c3acc31cc4d5548d91eb7d0de67479
Jefferson Lyrio <jefferson.lyrio@trescon.com.br>
2020-09-21 10:05:09 -0300

    Atualização dos modelos. Adicionando comentarios

--- ClienteEndereco.API/Persistence/Repositories/ClienteRepository.cs
+++ ClienteEndereco.API/Persistence/Repositories/ClienteRepository.cs
@@ -8,28 +8,68 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace ClienteEndereco.API.Persistence.Repositories {
+
+    //classse responsavel pela manipulação da entidade Cliente
     public class ClienteRepository : BaseRepository, IClienteRepository {
         public ClienteRepository(AppDbContext context) : base(context) { }
 
+        /*
+            Adiciona um novo Cliente
+            
+            insert into Cliente
+                (Identificador, Nome, Cpf, Idade, DataNascimento)
+            values
+                (cliente.Identificador, cliente.Nome, cliente.Cpf, cliente.Idade, cliente.DataNascimento)
+        */
         public async Task AddAsync(Cliente cliente) {
             await _context.Clientes.AddAsync(cliente);
             _context.SaveChanges();
         }
 
+
+        /*
+            Seleciona um Cliente especifico pelo id
+            
+            select * from Cliente c 
+            where c.Id = id
+        */
         public async Task<Cliente> FindByIdAsync(int id) {
             return await _context.Clientes.FindAsync(id);
         }
 
+
+        /*
+            Lista todos os Clientes
+            
+            select * from Cliente
+        */
         public async Task<IEnumerable<Cliente>> LIstAsync() {
             return await _context.Clientes.ToListAsync();
         }
 
+
+        /*
+            Remove um Cliente especificado pelo id
+            
+            delete Cliente c
+            where c.Id = id
+        */
         public async Task Remove(int id) {
             Cliente cliente = await FindByIdAsync(id);
             _context.Clientes.Remove(cliente);
             _context.SaveChanges();
         }
 
+        /*
+            Atualiza o Cliente
+
+            update Cliente c
+            set c.Identificador = cliente.Identificador
+                c.Nome = cliente.Nome
+                c.Cpf = cliente.Cpf
+                c.Idade = cliente.Idade
+                c.DataNascimento = cliente.DataNascimento
+         */
         public async Task Update(Cliente cliente) {            
             _context.Clientes.Update(cliente);
             _context.SaveChanges();
--- ClienteEndereco.API/Persistence/Repositories/EnderecoRepository.cs
+++ ClienteEndereco.API/Persistence/Repositories/EnderecoRepository.cs
@@ -8,28 +8,65 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace ClienteEndereco.API.Persistence.Repositories {
+
+    //classse responsavel pela manipulação da entidade Endereco
     public class EnderecoRepository : BaseRepository, IEnderecoRepository {
 
         public EnderecoRepository(AppDbContext context) : base(context) { }
+
+        /*
+            Adiciona um novo Endereço
+            
+            insert into Endereco
+                (Id, Logradura, Bairro, Cidade, Estado)
+            values
+                (endereco.Id, endereco.Logradura, endereco.Bairro, endereco.Cidade, endereco.Estado)
+        */
         public async Task AddAsync(Endereco endereco) {
             await _context.Enderecos.AddAsync(endereco);
             _context.SaveChanges();
         }
 
+        /*
+            Seleciona um endereço especifico pelo id
+            
+            select * from Endereco e 
+            where e.Id = id
+        */
         public async Task<Endereco> FindByIdAsync(int id) {
             return await _context.Enderecos.FindAsync(id);
         }
 
+        /*
+            Lista todos os endereços
+            
+            select * from Endereco
+        */
         public async Task<IEnumerable<Endereco>> LIstAsync() {
             return await _context.Enderecos.ToListAsync();
         }
 
+        /*
+            Remove um endereço especificado pelo id
+            
+            delete Endereco e
+            where e.Id = id
+        */
         public async Task Remove(int id) {
             Endereco endereco = await FindByIdAsync(id);
             _context.Remove(endereco);
             _context.SaveChanges();
         }
 
+        /*
+            Atualiza o endereço
+
+            update Endereco e
+            set e.Logradura = endereco.Logradura
+                e.Bairro = endereco.Bairro
+                e.Cidade = endereco.Cidade
+                e.Estado = endereco.Estado
+         */
         public async Task Update(Endereco endereco) {
             _context.Update(endereco);
             _context.SaveChanges();
